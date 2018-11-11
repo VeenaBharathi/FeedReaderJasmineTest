bba800b2c04f65ec59842501f87883767df08f8d
Veena Bharathi Akilla <veenabharathi93@gmail.com>
2018-11-11 17:35:39 +0530

    Urls not empty test case passed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it("urls are not empty", function(){
+            for(let feed of allFeeds){
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+                expect(feed.url).not.toBe(null);
+            }
+         });
 
 
         /* TODO: Write a test that loops through each feed
