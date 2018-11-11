a64a18827467ce22cb848ba8ae34edbb242d4a6d
Veena Bharathi Akilla <veenabharathi93@gmail.com>
2018-11-11 17:39:04 +0530

    names not empty test case passed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -44,6 +44,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it("names are not empty", function(){
+            for(let feed of allFeeds){
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+                expect(feed.name).not.toBe(null);
+            }
+         });
     });
 
 
